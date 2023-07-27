736faf9e1e62b363ddc96dcf273ef7ef9b1709ce
djememahamat <djeme.mahamat@gmail.com>
2023-07-27 12:46:11 +0000

    adding connection to api

--- src/SurveyComponent.jsx
+++ src/SurveyComponent.jsx
@@ -6,11 +6,27 @@ import "./index.css";
 import { json } from "./json";
 
 function SurveyComponent() {
-    const survey = new Model(json);
-    survey.onComplete.add((sender, options) => {
-        console.log(JSON.stringify(sender.data, null, 3));
-    });
-    return (<Survey model={survey} />);
+  const survey = new Model(json);
+  survey.onComplete.add((sender, options) => {
+    console.log(JSON.stringify(sender.data, null, 3));
+    console.log(process.env);
+    options.showSaveInProgress();
+    const xhr = new XMLHttpRequest();
+    xhr.open(
+      "POST",
+      "https://vhmpchntns.eu-west-1.awsapprunner.com/transfo_ui_save"
+    );
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.onload = xhr.onerror = function () {
+      if (xhr.status == 200) {
+        options.showSaveSuccess();
+      } else {
+        options.showSaveError();
+      }
+    };
+    xhr.send(JSON.stringify(sender.data));
+  });
+  return <Survey model={survey} />;
 }
 
 export default SurveyComponent;
